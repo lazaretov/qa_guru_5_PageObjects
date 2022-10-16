5736f02f6d795b7d5f8b50fa0bbf46a80c0bf4f6
Timofei <lazaretov03@gmail.com>
2022-10-16 19:35:30 +0600

    add all actions and checks

--- src/test/java/com/lazaretov/tests/RegistrationFormWithObjectsTests.java
+++ src/test/java/com/lazaretov/tests/RegistrationFormWithObjectsTests.java
@@ -5,11 +5,6 @@ import com.lazaretov.pages.RegistrationFormPage;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
-
-
 public class RegistrationFormWithObjectsTests {
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
     @BeforeAll
@@ -21,48 +16,32 @@ public class RegistrationFormWithObjectsTests {
 
     @Test
     void fillFormTest () {
-        String firstName = "Dude";
-        String lastName = "Random";
-        String email = "random@dude.ai";
-        String phone = "8800555353";
-        String subject = "English";
-        String state = "Haryana";
-        String city = "Karnal";
-        String addres = "textExample";
-        String hobby = "Reading";
-        String gender = "Female";
 
         registrationFormPage.openPage()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setEmail(email)
-                .setGender(gender);
+                .setFirstName("Dude")
+                .setLastName("Random")
+                .setEmail("random@dude.ai")
+                .setGender("Female")
+                .setPhone("8800555353")
+                .setDateOfBirth("28", "September", "1991")
+                .setSubjects("English")
+                .setHobbies("Reading")
+                .uploadPic("pic.JPG")
+                .setAddress("textExample", "Haryana", "Karnal")
+                .clickSubmit();
 
-        $("#userNumber").setValue(phone);
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption("September");
-        $(".react-datepicker__year-select").selectOption("1991");
-        $(".react-datepicker__day--028:not(.react-datepicker__day--outside-month").click();
-        $("#subjectsInput").setValue(subject).pressEnter();
-        $("#hobbiesWrapper").$(byText(hobby)).click();
-        $("#uploadPicture").uploadFromClasspath("pic.JPG");
-        $("#currentAddress").setValue(addres);
-        $("#react-select-3-input").setValue(state).pressEnter();
-        $("#react-select-4-input").setValue(city).pressEnter();
+        registrationFormPage.checkResultsTableVisible()
+                .checkResult("Student Name", "Dude Random")
+                .checkResult("Student Email", "random@dude.ai")
+                .checkResult("Gender", "Female")
+                .checkResult("Mobile", "8800555353")
+                .checkResult("Date of Birth", "28 September,1991")
+                .checkResult("Subjects", "English")
+                .checkResult("Hobbies", "Reading")
+                .checkResult("Picture", "pic.JPG")
+                .checkResult("Address", "textExample")
+                .checkResult("State and City", "Haryana Karnal");
 
 
-        $("#submit").click();
-
-        $(".modal-header").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").shouldHave(text(firstName + " " + lastName),
-                text(email),
-                text(gender),
-                text(phone),
-                text("28 September,1991"),
-                text(subject),
-                text(hobby),
-                text(addres),
-                text("pic.JPG"),
-                text(state + " " + city));
     }
 }
